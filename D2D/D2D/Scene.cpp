f87a07dd4b7c430daa7cd413ffa4bc411d713781
KS Kim <65766852+simeddk@users.noreply.github.com>
2023-11-03 14:28:21 +0900

    1103_Line

--- D2D/D2D/Scene.cpp
+++ D2D/D2D/Scene.cpp
@@ -1,30 +1,81 @@
 #include "Device.h"
 
+ID3D11Buffer* vertexBuffer = nullptr;
+ID3D11InputLayout* inputLayout = nullptr;
+
+struct Vertex
+{
+	D3DXVECTOR3 Position;
+};
 
 void InitScene()
 {
-	
+	//Vertex Data
+	Vertex vertices[2];
+	vertices[0].Position = D3DXVECTOR3(-0.5f, -0.5f, 0.f);
+	vertices[1].Position = D3DXVECTOR3(+0.5f, +0.5f, 0.f);
+
+	//Create Vertex Buffer
+	{
+		D3D11_BUFFER_DESC desc;
+		ZeroMemory(&desc, sizeof(D3D11_BUFFER_DESC));
+		desc.Usage = D3D11_USAGE_DEFAULT;
+		desc.ByteWidth = sizeof(Vertex) * 2;
+		desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+		D3D11_SUBRESOURCE_DATA subResource = { 0 };
+		subResource.pSysMem = vertices;
+		HRESULT result = Device->CreateBuffer(&desc, &subResource, &vertexBuffer);
+		assert(SUCCEEDED(result));
+	}
+
+	//Create InputLayout
+	{
+		D3D11_INPUT_ELEMENT_DESC desc;
+		desc.SemanticName = "POSITION";
+		desc.SemanticIndex = 0;
+		desc.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+		desc.InputSlot = 0;
+		desc.AlignedByteOffset = 0;
+		desc.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+		desc.InstanceDataStepRate = 0;
+
+		HRESULT result = Device->CreateInputLayout
+		(
+			&desc,
+			1,
+			VsBlob->GetBufferPointer(),
+			VsBlob->GetBufferSize(),
+			&inputLayout
+		);
+		assert(SUCCEEDED(result));
+	}
+
 }
 
 void DestroyScene()
 {
-
+	vertexBuffer->Release();
+	inputLayout->Release();
 }
 
 void Update()
 {
-	//충돌
-	//입력 이벤트
+	
 }
 
 void Render()
 {
-	//렌더링 명령(DP Call)
-	
 	D3DXCOLOR clearColor = D3DXCOLOR(0.15f, 0.15f, 0.15f, 1.f);
 	DeviceContext->ClearRenderTargetView(RTV, clearColor);
 	{
-		//DeviceContext->Draw()
+		UINT stride = sizeof(Vertex);
+		UINT offset = 0;
+		DeviceContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+		DeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
+		DeviceContext->IASetInputLayout(inputLayout);
+
+		DeviceContext->Draw(2, 0);
 	}
 	SwapChain->Present(0, 0);
 }
